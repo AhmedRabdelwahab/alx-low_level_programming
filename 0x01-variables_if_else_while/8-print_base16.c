568afd86e9051692d4e266c7a148742f138010b1
Your Name <you@example.com>
2023-10-16 15:32:06 -0700

    this my commit

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+* main - Prints a random number decides whether it is positive,negative,orzero.
+*
+* Return: Always 0.
+*/
+
+int main(void)
+{
+	char letter;
+
+	for (letter = '0'; letter <= 'f'; letter++)
+	{
+		putchar(letter);
+	}
+	for (letter = 'a'; letter <= 'f'; letter++)
+        {
+                putchar(letter);
+        }
+		putchar('\n');
+	return (0);
+}
