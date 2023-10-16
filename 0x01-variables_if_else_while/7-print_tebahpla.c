92c720511bb930df268fb2fde77f336c244b5b31
Your Name <you@example.com>
2023-10-16 15:16:51 -0700

    this my commit

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+* main - Prints a random number decides whether it is positive,negative,orzero.
+*
+* Return: Always 0.
+*/
+
+int main(void)
+{
+	char letter;
+	
+	for (letter = 'z'; letter <= 'a'; letter++)
+	{
+		putchar(letter);
+	}
+		putchar('\n');
+	return (0);
+}
