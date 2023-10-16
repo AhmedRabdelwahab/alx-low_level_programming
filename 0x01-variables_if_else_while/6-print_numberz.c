035fb2d28aef67c60502a38c72ccb8fbddfa4c6f
Your Name <you@example.com>
2023-10-16 15:06:35 -0700

    this my commit

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -6,11 +6,13 @@
  * Return: Always 0 (success)
  */
 
-int main()
+int main(void)
 {
 	int number;
 	for (number = '0' ; number <= '9' ; number++)
-		putchar(number);
+	{
+		putchar(number + '0');
 		putchar('\n');
+	}
 	return (0);
 }
