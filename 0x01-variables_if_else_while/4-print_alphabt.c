e33eff7b1f4717a6042392e96a46ea476442646b
Your Name <you@example.com>
2023-10-16 14:31:47 -0700

    this my commit

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+* main - Prints a random number decides whether it is positive,negative,orzero.
+*
+* Return: Always 0.
+*/
+
+int main(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z' ; letter++)
+	if (letter!='e'&&letter!='q')
+	{
+		putchar(letter);
+	}
+		putchar('\n');
+	return (0);
+}
