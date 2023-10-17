f90bd53e82c52cd6c915e42bde9989a08e1c81de
Your Name <you@example.com>
2023-10-17 12:44:12 -0700

    this my commit

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+/**
+* main - Prints a random number decides whether it is positive,negative,orzero.
+*
+* Return: Always 0.
+*/
+
+int main(void)
+{
+	int num1 = 0, num2, num3;
+
+	while (num1 <= 9)
+	{
+		num2 = 1;
+
+		while (num2 <= 9)
+		{
+			num3 = 2;
+			while (num3 <= 9)
+			{
+			
+			if (num1 != num2 && num2 != num3 && num1 < num2 && num2 < num3)
+			{
+				putchar(num1 + 48);
+				putchar(num2 + 48);
+				putchar(num3 + 48);
+					if (num1 + num2 + num3 != 24)
+					{
+						putchar(',');
+						putchar(' ');
+					}
+			}
+			num3++;
+		}
+		num2++;
+	}
+	num1++;
+}
+putchar('\n');
+return (0);
+}
