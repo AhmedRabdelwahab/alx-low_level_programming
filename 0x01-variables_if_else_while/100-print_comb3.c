2f893cef21f68c4136d61ea24408ba59b705d743
Your Name <you@example.com>
2023-10-17 12:03:42 -0700

    this my commit

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -8,14 +8,15 @@
 * Return: Always 0.
 */
 
-	int main(void)
-	{
+int main(void)
+{
 	int num1 = 0, num2;
 
 	while (num1 <= 9)
 	{
 		num2 = 0;
-		while (num2 <= 0)
+
+		while (num2 <= 9)
 		{
 			if (num1 != num2 && num1 < num2)
 			{
@@ -33,4 +34,4 @@
 	}
 		putchar('\n');
 		return (0);
-	}
+}
