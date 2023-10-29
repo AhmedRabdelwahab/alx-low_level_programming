2caf459d8d383ca14a6985db476b87301ce377df
Your Name <you@example.com>
2023-10-29 14:24:07 -0700

    this my commit

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,21 +1,18 @@
-#include <stdlib.h>
 #include "main.h"
 
 /**
-* main - Prints a random number decides whether it is positive,negative,orzero.
-*
-* Return: Always 0.
-*/
-
-
-int main()
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+int main(void)
 {
-	void print_alphabet(void);
-
+	print_alphabet();
 	char letter;
 
 	for (letter = 'a'; letter <= 'z'; letter++)
 	putchar(letter);
 
-        putchar('\n');
+	putchar('\n');
+	return (0);
 }
