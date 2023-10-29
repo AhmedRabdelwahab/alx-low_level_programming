bcd8a7aa5f3b76708a16f36975775b02382ac3f5
Your Name <you@example.com>
2023-10-29 14:20:33 -0700

    this my commit

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+* main - Prints a random number decides whether it is positive,negative,orzero.
+*
+* Return: Always 0.
+*/
+
+
+int main()
+{
+	void print_alphabet(void);
+
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+	putchar(letter);
+
+        putchar('\n');
+}
