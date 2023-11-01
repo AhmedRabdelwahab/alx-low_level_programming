f2e622cdadbb0008c42625b5362852e2636b41a3
Your Name <you@example.com>
2023-11-01 16:41:41 -0700

    this my commit

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+/**
+ * 24_hours - function
+ *
+ * @n: it is a variable
+ *
+ * Return: always 0
+ */
+
+void jack_bauer(void)
+{
+	int n , x;
+	for (n = 0; n <= 23; n++)
+	{
+		if (n <= 9)
+		{
+			_putchar('0');
+		}
+		else
+		{
+		_putchar(n);
+		_putchar(':');
+		}
+		for (x = 0; x <= 59; x++)
+		{
+			if (x <= 9)
+		{
+			_putchar('0');
+		}
+		else
+		{
+		_putchar(x);
+		}
+		}
+		_putchar('\n');
+		
+	}
+}
