e77064fefc40c266e99cbc0d0980d12c0fe84f4b
Your Name <you@example.com>
2023-11-01 12:23:42 -0700

    this my commit

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,20 +1,21 @@
 #include "main.h"
 
 /**
- * main - check the code.
+ * _islower - writes the character c to stdout
+ * @c: The character to print
  *
- * Return: Always 0.
+ * Return: On success 1.
+ *      On error, 0 is returned, and errno is set appropriately.
  */
-int main(void)
-{
-    int r;
 
-    r = _islower('H');
-    _putchar(r + '0');
-    r = _islower('o');
-    _putchar(r + '0');
-    r = _islower(108);
-    _putchar(r + '0');
-    _putchar('\n');
-    return (0);
+int _islower(int c)
+{
+	if (c >= 97 && c <= 122)
+	{
+		return 1; 
+	}
+	else 
+	{
+		return 0;
+	}
 }
