6af1ff057a286499be2e205012b159af0242d036
Your Name <you@example.com>
2023-11-01 09:43:57 -0700

    this my commit

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
 /**
- * 
- * print_alphabet_x10 - utilizes on the _putchar function to print
- *	
+ *
+ * print_alphabet_x10 - it prints the alphabet 10 times
+ *
  */
 
 void print_alphabet_x10(void)
