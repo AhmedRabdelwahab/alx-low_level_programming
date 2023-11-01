a06a0903cbf6fe771ad9223c00b98fd35060c4e1
Your Name <you@example.com>
2023-11-01 13:11:34 -0700

    this my commit

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * print_sign - check a spicific character
+ *
+ * @n: it should be as it should
+ *
+ * Return: 1 if it is greater than zero
+ * and 0 if it is zero
+ * and -1 if it is less than zero
+ */
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	if (n = 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	if (n < 0)
+	{
+		_putchar('-');
+		return (-1);
+	}
+
+}
