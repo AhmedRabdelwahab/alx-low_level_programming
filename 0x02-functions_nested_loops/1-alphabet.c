94861699dacac1c22288fc048533c51eb036d606
Your Name <you@example.com>
2023-11-01 06:41:53 -0700

    this my commit

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,18 +1,17 @@
+#include <stdio.h>
 #include "main.h"
 
-/**
- * main - check the code for Holberton School students.
- *
- * Return: Always 0.
- */
-int main(void)
+void print_alphabet(void)
 {
-	void print_alphabet(void);
-	char letter;
+        char letter = 97;
+        for(letter;letter <= 122;letter++)
 
-	for (letter = 'a'; letter <= 'z'; letter++)
-	putchar(letter);
+        {
+                putchar(letter);
+        }
+                putchar('\n');
 
-	putchar('\n');
-	return (0);
+
+
 }
+
