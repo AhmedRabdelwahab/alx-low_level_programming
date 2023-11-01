9045869a10f6c08bf2d37a0c5b94de03a5afa787
Your Name <you@example.com>
2023-11-01 13:49:32 -0700

    this my commit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_last_digit - it is the function name
+ *
+ * @x: it is the intger
+ *
+ * Return: always 0
+ */
+
+int print_last_digit(int x)
+{
+	x = x / 100;
+	return (x);
+}
