8848761b6d33bfe9211e57aeae26d2d9d7388b54
Your Name <you@example.com>
2023-11-01 12:53:28 -0700

    this my commit

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,22 +1,23 @@
 #include "main.h"
 
 /**
- * _isalpha - it is a programe that check the character and if it alphabetic it return 1
- * and it otherwise it returns 0
+ * _isalpha - it is a programe that check 
+ * the c character and if it alphabetic it return 1
+ * and it otherwise it return 0
  */
 
 int _isalpha(int c)
 {
 	if (c >= 65 && c <= 90)
 	{
-		return 1;
+		return (1);
 	}
 	else if (c >= 97 && c <= 122)
 	{
-		return 1;
+		return (1);
 	}
 	else
 	{
-		return 0;
+		return (0);
 	}
 }
