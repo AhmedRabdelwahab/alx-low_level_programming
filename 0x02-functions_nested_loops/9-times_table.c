9a05c63d5791e3d3f4596b3bc7ebfb72ebb0a9f0
Your Name <you@example.com>
2023-11-02 15:58:43 -0700

    this my commit

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -3,7 +3,7 @@
 /**
  * times_table - prints the 9 times table
  *
- * Example Table 
+ * Example Table
  * 0, 0, 0, 0, ...
  * 0, 1, 2, 3, ...
 */
@@ -15,7 +15,7 @@ void times_table(void)
 	for (num = 0; num <= 9; num++)
 	{
 		_putchar(48);
-	
+
 	for (mult = 1; mult <= 9; mult++)
 	{
 		_putchar(',');
