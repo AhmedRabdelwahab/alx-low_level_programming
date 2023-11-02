4d566fe634e5c6326425ee2628d6998e3a6810a4
Your Name <you@example.com>
2023-11-02 16:07:27 -0700

    this my commit

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * add - it is a function
+ *
+ * @x: it will be adding
+ * @z: it will be adding
+ * @n: it will be return
+ *
+ * Return: always 0
+ */
+
+int add(intx, intz)
+{
+	int n;
+
+	n = x + z;
+
+	printf("%d\n", n);
+	return (0);
+}
