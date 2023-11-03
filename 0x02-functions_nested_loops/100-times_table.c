8911c9da8809a21a2880c1d5d1035744bb13c95b
Your Name <you@example.com>
2023-11-03 10:02:57 -0700

    this my commit

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -31,7 +31,7 @@ void print_times_table(int n)
 		 * put space if product is a single number
 		 * place the first digit if its two numbers
 		 */
-		if (prod <= n * 10)
+		if (prod <= n * 2)
 			_putchar(' ');
 		else
 			_putchar((prod / 10) + 48); /*get the first digit*/
