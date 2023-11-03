eb2d958efbd9aa2b639fdc974a5880adf3808391
Your Name <you@example.com>
2023-11-03 09:57:48 -0700

    this my commit

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,41 @@
+#include "main.h"
+
+/**
+ * print_times_table - prints the n times table
+ *
+ * Example Table
+ * 0, 0, 0, 0, ...
+ * 0, 1, 2, 3, ...
+*/
+
+void print_times_table(int n)
+{
+	int num, mult, prod;
+
+	if (n <= 15 && n >= 0)
+	{
+	for (num = 0; num <= n; num++)
+	{
+		_putchar(48);
+
+	for (mult = 1; mult <= n; mult++)
+	{
+		_putchar(',');
+		_putchar(' ');
+
+		prod = num * mult;
+
+		/*
+		 * put space if product is a single number
+		 * place the first digit if its two numbers
+		 */
+		if (prod <= n)
+			_putchar(' ');
+		else
+			_putchar((prod / 10) + 48); /*get the first digit*/
+	_putchar((prod % 10) + 48); /*get the second digit*/
+	}
+	_putchar('\n');
+	}
+	}
+}
