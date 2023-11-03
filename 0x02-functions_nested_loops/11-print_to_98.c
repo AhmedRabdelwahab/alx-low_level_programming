f2c6b2300ad059872575f539f688af3f3ca4f8b1
Your Name <you@example.com>
2023-11-03 07:27:06 -0700

    this my commit

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_to_98 - is a function
+ *
+ * Return: always 0
+ */
+
+void print_to_98(int n)
+{
+	for (n = n; n <= 98; n++)
+	{
+		printf("%d\n", n);
+	}
+}
