13b5e2dab864c1eb88c938d8a674c47552394df5
Your Name <you@example.com>
2023-11-03 07:36:46 -0700

    this my commit

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -15,14 +15,14 @@ void print_to_98(int n)
 	{
 		for (n = n; n <= 98; n++)
 		{
-			printf("%d\n", n)
+			printf("%d\n", n);
 		}
 	}
-	if else (n > 98)
+	else if (n > 98)
 	{
 		for (n = n; n >= 98; n--)
 		{
-			printf("%d\n", n)
+			printf("%d\n", n);
 		}
 	}
 }
