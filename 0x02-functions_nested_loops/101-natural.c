7f4802a23324a898d158a083ae8b04c89dcb5f23
Your Name <you@example.com>
2023-11-04 10:52:19 -0700

    this my commit

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - a function
+ *
+ * @n: a variable
+ * @a: a variable
+ * @b: a variable
+ */
+
+int main(int n)
+{
+	for (n = 1024; n >= 0; n--)
+	{
+		int a = n % 5;
+		int b = n % 3;
+
+		if (a == 0 || b == 0)
+		{
+			printf("%d\n", n);
+		}
+	}
+}
