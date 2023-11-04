2633938bc5df07a67ba479dcb22cfa303e036ecc
Your Name <you@example.com>
2023-11-04 16:49:12 -0700

    this my commit

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -11,7 +11,7 @@ int main(void)
 	int count;
 	unsigned long int fib1 = 0, fib2 = 1, sum;
 
-	for (count = 0; count < 98; count++)
+	for (count = 0; count < 95; count++)
 	{
 		sum = fib1 + fib2;
 		printf("%lu", sum);
@@ -19,7 +19,7 @@ int main(void)
 		fib1 = fib2;
 		fib2 = sum;
 
-		if (count == 97)
+		if (count == 94)
 		{
 			printf("\n");
 		}
