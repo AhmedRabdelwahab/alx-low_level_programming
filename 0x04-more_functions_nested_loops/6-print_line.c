4fea6f58bf3a255f84da958560212af142a6be40
Your Name <you@example.com>
2023-11-12 14:33:35 -0800

    this my commit

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_line - function
+ * 
+ * @n: is a variable
+ *
+ * Return: Always 0
+ */
+
+void print_line(int n)
+{
+	int co;
+
+	for (co = 0; co <= n; co++)
+	{
+		if (n != 0)
+		{
+			_putchar('_');
+		}
+		_putchar('\n');
+	}
+}
