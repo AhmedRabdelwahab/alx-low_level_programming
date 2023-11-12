7fa17e4723a327eb3e834565d5073dbb0b4b54da
Your Name <you@example.com>
2023-11-12 10:57:28 -0800

    this my commit

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -5,18 +5,13 @@
  *
  * @c: variable
  *
- * Return: Returns 1 if c is uppercase
- *       Returns 0 otherwise
+ * Return: Returns 1 if c is uppercase, Returns 0 otherwise
  */
 
 int _isupper(int c)
 {
 	if (c >= 56 && c <= 90)
-	{
 		return (1);
-	}
 	else
-	{
 		return (0);
-	}
 }
