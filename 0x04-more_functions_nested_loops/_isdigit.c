503a747ebd878d22ec977822b19a166db123e1c9
Your Name <you@example.com>
2023-11-12 11:06:43 -0800

    this my commit

---
+++ 0x04-more_functions_nested_loops/_isdigit.c
@@ -0,0 +1,14 @@
+#include <unistd.h>
+#include "main.h"
+
+/**
+ * _isupper - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ *	On error, 0 is returned, and errno is set appropriately.
+ */
+	int _isdigit(int c)
+	{
+		return (write(1, &c, 1));
+	}
