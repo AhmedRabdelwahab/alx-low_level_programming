61455af475f8c90d345ca5a0bf858a6d20357e6f
Your Name <you@example.com>
2023-11-12 11:26:34 -0800

    this my commit

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_numbers - is a function
+ *
+ * @n: is a variable
+ */
+
+void print_numbers(void)
+{
+	int n;
+	for (n = 48; n <= 57; n++)
+	{
+		_putchar(n);
+		_putchar('\n');
+	}
+}
