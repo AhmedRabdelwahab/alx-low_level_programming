71a14dd553eca45ff2aa59e5de7da3ac8845064d
Your Name <you@example.com>
2023-11-13 07:39:14 -0800

    this my commit

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -12,25 +12,15 @@ int main(int count)
 	for (count = 1; count <= 100; count++)
 	{
 		if (count % 3 == 0 && !(count % 5 == 0))
-		{
 			printf("Fizz");
-		}
 		if (count % 5 == 0 && !(count % 3 == 0))
-		{
 			printf("Buzz");
-		}
 		if (count % 3 == 0 && count % 5 == 0)
-		{
 			printf("FizzBuzz");
-		}
 		else
-		{
 			printf("%d", count);
-		}
 		if (count != 100)
-		{
 		printf(" ");
-		}
 	}
 	printf("\n");
 	return (0);
