6cbbe08210f6cea889df52a79275b86ac589cf1d
Your Name <you@example.com>
2023-11-14 23:19:38 -0800

    this my commit

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,23 +1,24 @@
 #include "main.h"
 
 /**
- * print_number - function
+ * print_number - function.
  *
- * @n: variable
- */
+ * @n: variable.
+*/
 
 void print_number(int n)
 {
 	unsigned int num = n;
 
+	/*zero*/
 	if (n < 0)
 	{
 		_putchar('-');
 		num = -num;
 	}
-
+	/*one*/
 	if ((num / 10) > 0)
 		print_number(num / 10);
-
+	/*two*/
 	_putchar((num % 10) + 48)
 }
