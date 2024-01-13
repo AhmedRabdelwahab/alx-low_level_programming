6708dce1675edbf8bb955463487741d3e41cc75c
AhmedRabdelwahab <ahmedrahababdelwahab@gmail.com>
2024-01-13 13:59:21 -0800

    this my commit

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - function is the intry point
+ * this program prints text
+ * Return: Always 1
+ */
+int main(void)
+{
+	puts("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+	return (1);
+}
